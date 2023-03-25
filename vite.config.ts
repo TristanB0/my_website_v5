cc54abac05562c07be0df6b18e616291611afecc
Tristan BONY <tristan.bony@hotmail.fr>
2023-03-25 02:04:07 +0100

    /!\ Cloudflare plugin issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import cloudflare from 'solid-start-cloudflare-pages';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin({ adapter: cloudflare({}) })],
   server: {
     port: 3000,
   },
