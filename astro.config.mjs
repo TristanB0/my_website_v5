8a2a5b92381def620e53d000b772589138b26083
Tristan Bony <tristan.bony@hotmail.fr>
2023-07-24 01:17:09 +0200

    Created structure Astro+SolidJS+Tailwind+Vercel

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+    output: "server",
+    adapter: vercel({
+        analytics: true,
+    }),
+    integrations: [tailwind(), solidJs()]
+});
