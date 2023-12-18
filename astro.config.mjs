5365bd44b29aecdaed48313e4c0dab22030ee185
Tristan Bony <tristan.bony@hotmail.fr>
2023-12-18 01:12:52 -0600

    Beginning of posts implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), mdx()],
   adapter: cloudflare()
 });
 
 /*
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), solidJs()],
+  integrations: [tailwind(), solidJs(), ndx()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
