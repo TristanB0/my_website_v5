6af465ffc41c636144ea930757e53d8ef40a4c73
Tristan Bony <tristan.bony@hotmail.fr>
2024-01-07 00:09:20 -0600

    Remove SolidJS, update packages, begin ViewTransitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 //import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), solidJs(), mdx()],
+  integrations: [tailwind(), mdx()],
   adapter: cloudflare()
 });
 
