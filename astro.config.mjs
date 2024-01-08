0f2a16343e54f5a811db534f825c1e3dcc83e949
Tristan Bony <tristan.bony@hotmail.fr>
2024-01-08 00:11:55 -0600

    Remove Cloudflare for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 import { defineConfig } from 'astro/config';
-//import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  integrations: [tailwind(), mdx()],
-  adapter: cloudflare()
-});
-
-/*
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), mdx()],
@@ -24,4 +15,3 @@ export default defineConfig({
     },
   }),
 });
-*/
