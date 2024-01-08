ae4242033a0fc4917dff261fd4f2d60867205dae
Tristan Bony <tristan.bony@hotmail.fr>
2024-01-07 18:47:48 -0600

    Display MDX nicely

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,11 @@ module.exports = {
 	theme: {
 		extend: {}
 	},
-	plugins: [require("daisyui")],
+	plugins: [
+		require("daisyui"),
+		require('@tailwindcss/typography')
+	],
 	daisyui: {
-		themes: [ "light", "dark" ]
+		themes: ["light", "dark"]
 	}
 }
