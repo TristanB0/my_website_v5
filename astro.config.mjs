56ffb978b015f44487662722d5aafca052f2abb4
Tristan Bony <tristan.bony@hotmail.fr>
2024-01-30 10:37:50 -0600

    Improve SVG rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [tailwind(), mdx()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: false
-    },
-    speedInsights: {
-      enabled: false
-    },
-    imageService: true
-  })
+    output: "server",
+    integrations: [
+        tailwind(),
+        mdx(),
+        icon({
+            include: {
+                "material-symbols": ["sunny-outline-rounded", "dark-mode-outline-rounded", "location-on-outline-rounded", "phone-android-outline-rounded", "school-outline-rounded", "mail-outline-rounded", "sim-card-download-outline-rounded"]
+            },
+            iconDir: "src/assets/icons",
+        })
+    ],
+    adapter: vercel({
+        webAnalytics: {
+            enabled: false
+        },
+        speedInsights: {
+            enabled: false
+        },
+        imageService: true
+    })
 });
