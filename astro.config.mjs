25ae34026060904792e0ff8f57fc3ed57c7f3172
Tristan Bony <tristan.bony@hotmail.fr>
2025-01-27 01:57:39 +0100

    Update Astro and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
@@ -7,6 +7,9 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  prefetch: {
+    prefetchAll: true,
+  },
   integrations: [
     tailwind(),
     mdx(),
