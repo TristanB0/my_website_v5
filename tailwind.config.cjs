2facf1dd2dff1de4b05965d4c4b3841424026cf8
Tristan Bony <git@tristanbony.me>
2026-02-03 16:56:15 +0100

    Upgrade

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	darkMode: 'class',
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		require("daisyui"),
-		require('@tailwindcss/typography')
-	],
-	daisyui: {
-		themes: ["light", "dark"]
-	}
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
